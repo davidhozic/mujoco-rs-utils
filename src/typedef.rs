9d6066f67ba5164151889a44bd7a43038a0dfffd
David Hozic <davidhozic@gmail.com>
2025-09-23 14:18:14 +0200

    Process comments

--- src/typedef.rs
+++ src/typedef.rs
@@ -7,9 +7,14 @@ use regex;
 pub fn create_types(api_reference: &Path, prefix: Option<&str>) {
     let filedata = fs::read_to_string(api_reference).unwrap();
     let struct_pat = prefix.unwrap_or("");
-    let re = regex::Regex::new(&format!(r"\.\.\s+mujoco-include::\s*({struct_pat}[A-z]+)")).unwrap();
+    let re = regex::Regex::new(&format!(r"(\w+[\w\s`:<> \->.]*?)\.\.\s+mujoco-include::\s*({struct_pat}[A-z]+)")).unwrap();
     for captures in re.captures_iter(&filedata) {
-        let name = captures.get(1).unwrap().as_str().trim_end_matches("_");
-        println!("type {} = {};", name.to_pascal_case(), name);
+        let docstring = captures.get(1).unwrap().as_str().trim();
+
+        let name = captures.get(2).unwrap().as_str().trim_end_matches("_");
+        for line in docstring.lines() {
+            println!("/// {line}");
+        }
+        println!("type {} = {};\n", name.to_pascal_case(), name);
     }
 }
