a7bf5b619b6af4daff6d8a451b62ba03c2e6476a
David Hozic <davidhozic@gmail.com>
2025-09-23 21:42:01 +0200

    Fix comment matching

--- src/typedef.rs
+++ src/typedef.rs
@@ -7,7 +7,7 @@ use regex;
 pub fn create_types(api_reference: &Path, prefix: Option<&str>) {
     let filedata = fs::read_to_string(api_reference).unwrap();
     let struct_pat = prefix.unwrap_or("");
-    let re = regex::Regex::new(&format!(r"(\w+[\w\s`:<> \->.]*?)\.\.\s+mujoco-include::\s*({struct_pat}[A-z]+)")).unwrap();
+    let re = regex::Regex::new(&format!(r"(\w+[^~]*?)\.\.\s+mujoco-include::\s*({struct_pat}[A-z]+)")).unwrap();
     for captures in re.captures_iter(&filedata) {
         let docstring = captures.get(1).unwrap().as_str().trim();
 
