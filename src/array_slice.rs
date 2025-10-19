8c28f1dfe6fb424efbbfba60a003c33498be59dd
David Hozic <davidhozic@gmail.com>
2025-10-19 19:13:50 +0200

    Enum handling

--- src/array_slice.rs
+++ src/array_slice.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use std::path::Path;
 use std::fs;
 use inflector::Inflector;
-use regex;
+use regex::{self, Regex};
 
 
 const C_TO_RUST_TYPE_MAPPING: LazyCell<HashMap<&'static str, &'static str>> = LazyCell::new(
@@ -17,6 +17,8 @@ const C_TO_RUST_TYPE_MAPPING: LazyCell<HashMap<&'static str, &'static str>> = La
     ])
 );
 
+const ENUM_REGEX: LazyCell<Regex> = LazyCell::new(|| Regex::new(r"(.*?)\((mjt[A-z]+)\)").unwrap());
+
 
 fn display_parsed(datatype: &str, name: &str, comment: &str, size_var: &str, size_mul: &str, accessor_prefix: &str) {
     // Convert to pascal case whenever the type starts with 'mj' or try to map to a Rust type.
@@ -29,12 +31,29 @@ fn display_parsed(datatype: &str, name: &str, comment: &str, size_var: &str, siz
 
     // Create an array type if size is larger than one, otherwise assume scalar
     if size_mul == "1" || size_mul.len() == 0 {
-        println!("{name}: &[{datatype}; \"{}\"; {accessor_prefix}.{size_var}],", comment.trim());
+        println!(
+            "{name}: &[{datatype}{}; \"{}\"; {accessor_prefix}.{size_var}],",
+            if datatype.starts_with("Mjt") && datatype != "MjtNum" && datatype != "MjtByte" {" [cast]"} else {""},
+            comment.trim()
+        );
     }
     else {
-        println!("{name}: &[[{datatype}; {size_mul}]; \"{}\"; {accessor_prefix}.{size_var}],", comment.trim());
+        println!("{name}: &[[{datatype}; {size_mul} as usize] [cast]; \"{}\"; {accessor_prefix}.{size_var}],", comment.trim());
     }
 }
+
+/// Try to extract an enum type from the documentation string.
+/// E.g., dynamics type (mjtDyn) gets converted to ("dynamics type", "mjtDyn").
+fn extract_possible_enum<'a>(docstring: &'a str, datatype: &'a str) -> (&'a str, &'a str) {
+    if let Some(m) = ENUM_REGEX.captures(docstring) {
+        let (_, [comment, enum_type]) = m.extract();
+        (comment, enum_type)
+    }
+    else {
+        (docstring, datatype)
+    }
+}
+
 
 pub fn create_array_slice(structs_filepath: &Path, accessor_prefix: &str, struct_name: &str) {
     let filedata = fs::read_to_string(structs_filepath).unwrap();
@@ -43,16 +62,26 @@ pub fn create_array_slice(structs_filepath: &Path, accessor_prefix: &str, struct
     let struct_data = re.find(&filedata).expect("failed to find struct or struct body").as_str();
 
     // Match the sizes that are marked with n something x some number
-    let re = regex::Regex::new(r"((?:unsigned\s+)?[^\s]+)(?:\*\s+([^\s]+)|\s+([^\s]+)\[.+\]);\s+//(.*)\(([A-z]+)\s*x\s*([0-9]+)\)").unwrap();
+    let re = regex::Regex::new(r"(?m)((?:unsigned\s+)?[^\s]+)(?:\*\s+([^\s]+)|\s+([^\s]+)\[.+\]);\s+//(.*)\(([A-z]+)\s*(?:x|\*)\s*(\w+)\)$").unwrap();
     for capture in re.captures_iter(struct_data) {
-        let (_, [datatype, name, comment, size_var, size_mul]) = capture.extract();
+        let (_, [mut datatype, name, mut comment, size_var, size_mul]) = capture.extract();
+        (comment, datatype) = extract_possible_enum(comment, datatype);
         display_parsed(datatype, name, comment, size_var, size_mul, accessor_prefix);
     }
 
-    // Match the sizes that are marked either with n something or some number x n something
-    let re = regex::Regex::new(r"((?:unsigned\s+)?[^\s]+)(?:\*\s+([^\s]+)|\s+([^\s]+)\[.+\]);\s+//(.*)\(([0-9]*)\s*\*?\s*([A-z]+)\)").unwrap();
+    // Match the sizes that are marked with some number * n something
+    let re = regex::Regex::new(r"(?m)((?:unsigned\s+)?[^\s]+)(?:\*\s+([^\s]+)|\s+([^\s]+)\[.+\]);\s+//(.*)\(([0-9]*)\s*(?:\*|x)\s*(\w+)\)$").unwrap();
     for capture in re.captures_iter(struct_data) {
-        let (_, [datatype, name, comment, size_mul, size_var]) = capture.extract();
+        let (_, [mut datatype, name, mut comment, size_mul, size_var]) = capture.extract();
+        (comment, datatype) = extract_possible_enum(comment, datatype);
         display_parsed(datatype, name, comment, size_var, size_mul, accessor_prefix);
+    }
+
+    // Match the sizes that are marked with some fixed attribute for length
+    let re = regex::Regex::new(r"(?m)((?:unsigned\s+)?[^\s]+)(?:\*\s+([^\s]+));\s+//(.*)\((\w+)\)$").unwrap();
+    for capture in re.captures_iter(struct_data) {
+        let (_, [mut datatype, name, mut comment, size_var]) = capture.extract();
+        (comment, datatype) = extract_possible_enum(comment, datatype);
+        display_parsed(datatype, name, comment, size_var, "", accessor_prefix);
     }
 }
